093f60fbda73ed9765a0d9671fa316e8331b1226
Alexander Bukin <lxndrbukin@proton.me>
2026-02-28 21:57:45 +0000

    Work on refactor from class to functional components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,65 +1,44 @@
 import './assets/styles.scss';
-import { Component } from 'react';
-import { AppProps } from './types';
-import { connect } from 'react-redux';
-import { RootState, WeatherProps } from '../store';
-import { Header } from './Header/Header';
-import { CurrentWeather } from './CurrentWeather/CurrentWeather';
+import { type JSX } from 'react';
+import { useSelector } from 'react-redux';
+import { type RootState } from '../store';
+import Header from './Header/Header';
+import CurrentWeather from './CurrentWeather/CurrentWeather';
 import { Forecast } from './Forecast/Forecast';
-import { Footer } from './Footer/Footer';
+import Footer from './Footer/Footer';
+import Summary from './Summary/Summary';
 
-class _App extends Component<AppProps> {
-  constructor(props: AppProps) {
-    super(props);
-    this.renderCurrent = this.renderCurrent.bind(this);
-    this.renderForecast = this.renderForecast.bind(this);
-  }
+export default function App(): JSX.Element {
+  const { loading, weather } = useSelector((state: RootState) => state.weatherData);
 
-  componentDidMount(): void {
-    window.document.title = 'Weatherly';
-  }
-
-  renderCurrent(): JSX.Element | null {
-    const { current } = this.props.weather;
+  const currentWeather = (): JSX.Element | null => {
+    const { current } = weather;
     if (current) {
-      return (
-        <CurrentWeather
-          city={current.name}
-          weather={current.weather[0]}
-          hwp={{ main: current.main, wind: current.wind }}
-          sys={current.sys}
-        />
-      );
+      return <CurrentWeather
+        city={current.name}
+        weather={current.weather[ 0 ]}
+        hwp={{ main: current.main, wind: current.wind }}
+        sys={current.sys}
+      />;
     }
     return null;
-  }
+  };
 
-  renderForecast() {
-    const { forecast } = this.props.weather;
+  const forecast = (): JSX.Element | null => {
+    const { forecast } = weather;
     if (forecast) {
       return <Forecast list={forecast.list} />;
     }
-  }
-
-  render(): JSX.Element {
-    return (
-      <div className="container">
-        <Header />
-        {!this.props.loading && this.renderCurrent()}
-        {!this.props.loading && this.renderForecast()}
-        <Footer />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = ({
-  weatherData,
-}: RootState): { weather: WeatherProps; loading: boolean } => {
-  return {
-    weather: weatherData.weather,
-    loading: weatherData.loading,
+    return null;
   };
-};
 
-export const App = connect(mapStateToProps, {})(_App);
+  return (
+    <div className="container">
+      <Header />
+      {loading && currentWeather()}
+      {loading && forecast()}
+      <Summary />
+      <Footer />
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { App } from './components/App';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.querySelector('#root')!);
 
